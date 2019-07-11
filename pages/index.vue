5adf982cdebeeeea497a1c1a61d9c4b3c81ec090
rokuzeudon <rokuzeudon@gmail.com>
2019-07-11 11:23:03 +0900

    Try pug

--- pages/index.vue
+++ pages/index.vue
@@ -1,31 +1,13 @@
-<template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
+<template lang="pug">
+  .container
+    div
+      logo
+      h1.title
         hatenablog-theme-builder
-      </h1>
-      <h2 class="subtitle">
-        My marvelous Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
-    </div>
-  </div>
+      h2.subtitle My marvelous Nuxt.js project
+      .links
+        a.button--green(href="https://nuxtjs.org/" target="_blank") Documentation
+        a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank") GitHub
 </template>
 
 <script>
