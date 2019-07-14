3e198a959bc6279b8e42615c06a539f34da713f4
rokuzeudon <rokuzeudon@gmail.com>
2019-07-14 09:06:41 +0900

    Remove Logo.vue

--- pages/index.vue
+++ pages/index.vue
@@ -9,17 +9,3 @@
         a.button--green(href="https://nuxtjs.org/" target="_blank") Documentation
         a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank") GitHub
 </template>
-
-<script>
-import Logo from '~/components/Logo.vue'
-
-export default {
-  components: {
-    Logo
-  }
-}
-</script>
-
-<style>
-
-</style>
