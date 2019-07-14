cea68827d561dc72dc5af51c768df9cd949c2a8d
rokuzeudon <rokuzeudon@gmail.com>
2019-07-14 13:57:02 +0900

    Remove default nuxt.js setting and files

--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,3 @@
 <template lang="pug">
-  .container
-    div
-      logo
-      h1.title
-        hatenablog-theme-builder
-      h2.subtitle My marvelous Nuxt.js project
-      .links
-        a.button--green(href="https://nuxtjs.org/" target="_blank") Documentation
-        a.button--grey(href="https://github.com/nuxt/nuxt.js" target="_blank") GitHub
+
 </template>
